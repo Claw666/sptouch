7979c8b4858dfb9bbd2b605e3add5af7adb35813
Csongor Kocsis <Csongor Kocsis>
2017-04-26 16:43:52 +0100

    deleted homepage buttons animation

--- assets/js/grow.js
+++
@@ -1,9 +0,0 @@
-var elements = document.querySelectorAll('.box')
-var animationDuration = 50000; // in milliseconds
-
-// Set the animationDelay of each element to a random value
-// between 0 and animationDuration:
-for (var i = 0; i < elements.length; i++) {
-  var randomDuration = Math.floor(Math.random() * animationDuration);
-  elements[i].style.animationDelay = randomDuration + 'ms';  
-}
